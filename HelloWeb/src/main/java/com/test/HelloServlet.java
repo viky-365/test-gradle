37157ea0a58ca665bd8709c40582f29d0c722bce
viky <vikrantk365@gmail.com>
2014-05-01 07:13:18 +0100

    simple web app with gradle

---
+++ HelloWeb/src/main/java/com/test/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		PrintWriter out = res.getWriter();
+		out.print("Hello Gradle 4");
+		out.close();
+	}
+
+}
---
+++ HelloWeb/src/test/java/com/test/HelloServetTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HelloServetTest {
+
+	@Test
+	public void dummy_test(){
+		assertTrue(true);
+	}
+}
